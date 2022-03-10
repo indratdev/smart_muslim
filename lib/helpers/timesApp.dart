3d1887eb22de1761bd3888988a660d664ce3c323
indratdev <indrat.dev@gmail.com>
2022-03-10 07:12:56 +0700

    penambahan fungsi untuk pembanding waktu

--- lib/helpers/timesApp.dart
+++ lib/helpers/timesApp.dart
@@ -2,31 +2,51 @@ import 'package:intl/intl.dart';
 import 'package:smart_muslim/models/datetimes_pray.dart';
 
 class TimesApp {
-  void getCurrentTime() {
-    var time = DateFormat("HH:mm").format(DateTime.now());
-    var time2 =
-        DateFormat("HH:mm").format(DateTime.now().add(Duration(minutes: 60)));
-    print('times : $time');
-    print('times : $time2');
+  String currentTime = "Na:Na";
+  String nextTime = "Na:Na";
 
-    // var ttime = time.split(':');
-    // var ttime2 = time2.split(':');
+  // benerin yang disini
 
-    // var totalSeconds1 = parseInt(ttime[0] * 3600 + ttime[1] * 60 + ttime[0]);
-    // var totalSeconds2 = parseInt(ttime2[0] * 3600 + ttime2[1] * 60 + ttime2[0]);
+  String checkTimeFirst(List<String> listWaktu, String waktuSekarang) {
+    String result = "";
+    int target = int.parse(waktuSekarang.replaceAll(":", ""));
+
+    for (var value in listWaktu) {
+      var data = int.parse(value.replaceAll(":", ""));
+      if (data > target) {
+//        print("data ini lebih tingggi $data");
+        result = data.toString();
+        break;
+      }
+    }
+    return result;
   }
 
-  // compareTime(str1, str2){
-  compareTime() {
-    // var str1 = DateFormat("HH:mm").format(DateTime.now());
-    // var str2 =
-    //     DateFormat("HH:mm").format(DateTime.now().add(Duration(minutes: 60)));
+  // void getCurrentTime() {
+  //   var time = DateFormat("HH:mm").format(DateTime.now());
+  //   var time2 =
+  //       DateFormat("HH:mm").format(DateTime.now().add(Duration(minutes: 60)));
+  //   print('times : $time');
+  //   print('times : $time2');
 
-    // str1.replaceAll(":", "");
-    // print(str1.replaceAll(":", ""));
+  //   // var ttime = time.split(':');
+  //   // var ttime2 = time2.split(':');
 
-    Times datetime;
+  //   // var totalSeconds1 = parseInt(ttime[0] * 3600 + ttime[1] * 60 + ttime[0]);
+  //   // var totalSeconds2 = parseInt(ttime2[0] * 3600 + ttime2[1] * 60 + ttime2[0]);
+  // }
 
-    // print(datetime);
-  }
+  // // compareTime(str1, str2){
+  // compareTime() {
+  //   // var str1 = DateFormat("HH:mm").format(DateTime.now());
+  //   // var str2 =
+  //   //     DateFormat("HH:mm").format(DateTime.now().add(Duration(minutes: 60)));
+
+  //   // str1.replaceAll(":", "");
+  //   // print(str1.replaceAll(":", ""));
+
+  //   Times datetime;
+
+  //   // print(datetime);
+  // }
 }
